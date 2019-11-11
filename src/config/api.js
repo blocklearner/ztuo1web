1979aff701e6c9d6c07051e261e997c3aa09ceb7
taotao820 <xht820@gmail.com>
2019-11-11 19:36:24 +0800

    commit

--- src/config/api.js
+++ src/config/api.js
@@ -1,45 +1,30 @@
 export default {
+    host: 'http://127.0.0.1/8090',
     common: {
-        area: '/uc/support/country'
+        login: '/admin/system/employee/sign/in'
     },
-    uc: { //会员中心接口
-        login: '/uc/login',
-        register: '/uc/register',
-        wallet: '/uc/asset/wallet/',
-        captcha: '/uc/start/captcha',
-        identification: '/uc/approve/certified/business/status', //商家认证
-        apply: '/uc/approve/certified/business/apply', //商家认证申请
-        announcement: '/uc/announcement/page', //公告列表
-        // paydividends:"/uc/bonus/user/",//持币分红
-        paydividends: "/uc/bonus/user/page", //持币分红
-        mylist: '/uc/mine/list_es', //交易挖矿
-        mylistrecord: "/uc/mine/detail/", //交易挖矿详情
+    system: {
+        permissionAll: '/admin/system/role/permission/all',
+        role: '/admin/system/role/all',
+        rolePermission: '/admin/system/role/permission',
+        statistics: '/admin/system/statistics/dashboard'
     },
-    market: { //币币交易行情接口
-        ws: '/market/market-ws',
-        thumb: '/market/symbol-thumb',
-        thumbTrend: '/market/symbol-thumb-trend',
-        plate: '/market/exchange-plate', //主动查询的盘口信息
-        platemini: '/market/exchange-plate-mini', //获取10条数据
-        platefull: '/market/exchange-plate-full', //获取所有数据
-        trade: '/market/latest-trade', //主动查询的实时成交信息
-        symbolInfo: '/market/symbol-info',
-        indexData: "/market/index_info"
-    },
-    exchange: { //币币交易委托提交与查询接口
-        orderAdd: '/exchange/order/add', //提交订单接口
-        current: '/exchange/order/current', //当前委托接口
-        history: '/exchange/order/history', //历史委托接口
-        detail: '/exchange/order/detail/', //详细订单接口
-        favorFind: '/exchange/favor/find', //查询自选
-        favorAdd: '/exchange/favor/add', //添加自选
-        favorDelete: '/exchange/favor/delete', //删除自选
-        orderCancel: '/exchange/order/cancel' //取消委托
-    },
-    otc: {
-        coin: '/otc/coin/all', //查询支持的币种
-        advertise: '/otc/advertise/page-by-unit', //获取广告
-        createAd: '/uc/ad/create', //创建广告
-        adDetail: '/otc/advertise/detail'
+    otc:{
+        //法币币种管理
+        OtcCoin: '/admin/otc/otc-coin/page-query',
+        OtcCoinDetail:'admin/otc/otc-coin/detail',
+        update: 'admin/otc/otc-coin/update', 
+        create: 'admin/otc/otc-coin/create',
+        //法币广告管理
+        advertise:'admin/otc/advertise/page-query',
+        advertiseStatus:'admin/otc/advertise/alter-status',
+        //法币订单管理
+        order:'admin/otc/order/page-query',
+        orderDetail:'admin/otc/order/detail',
+        //法币申诉管理
+        appeal:'admin/otc/appeal/page-query',
+        detail:'admin/otc/appeal/detail',
+        cancelOrder:'admin/otc/appeal/cancel-order',//取消订单
+        releaseCoin: 'admin/otc/appeal/release-coin',//放币
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,84 +1,23 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import VueClipboard from 'vue-clipboard2'
-import routes from './config/routes.js';
-import store from './config/store.js';
-import vueResource from 'vue-resource'
+import Vue from 'vue';
+import iView from 'iview';
+import { router, initRouter } from './router/index';
+import { appRouter } from './router/router';
+import store from './store';
+import App from './app.vue';
+import '@/locale';
+import 'iview/dist/styles/iview.css';
 import VueI18n from 'vue-i18n';
-import util from './assets/js/util.js'; //乘除方法，js的算数运算真是SB
+import util from '@/libs/util';
+import axios from 'axios';
+import qs from 'qs';
+import Api from '@/config/api';
+import Cookies from 'js-cookie';
+import Viser from 'viser-vue';
+import { setStore, getStore, removeStore } from "@/config/storage.js";
 
-import App from './App.vue';
-import Api from './config/api';
-// import $ from '@js/jquery.min.js';
-var moment = require('moment');
+Vue.prototype.$ajax = axios;
+axios.defaults.withCredentials = true;
 
-// Vue.use(iView);
-Vue.use(VueClipboard)
-Vue.use(VueRouter);
-Vue.use(vueResource);
-Vue.use(VueI18n);
-
-
-// Vue.prototype.host = "http://p2586e5695.wicp.vip:17348"; //外网
-Vue.prototype.host = "http://192.168.31.61:80"; // 第一套
-
-// Vue.prototype.host = "https://manager.coinmany.com"; // 第二套
-Vue.prototype.api = Api;
-Vue.http.options.credentials = true;
-Vue.http.options.emulateJSON = true;
-Vue.http.options.headers = {
-    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-    // 'Content-Type': 'application/json;charset=utf-8'
-};
-const router = new VueRouter({
-    mode: 'hash',
-    routes
-})
-const i18n = new VueI18n({
-    locale: 'zh', // 语言标识
-    messages: {
-        'zh': require('./assets/lang/zh.js'),
-        'en': require('./assets/lang/en.js')
-    }
-});
-Vue.http.interceptors.push((request, next) => {
-    //登录成功后将后台返回的TOKEN在本地存下来,每次请求从sessionStorage中拿到存储的TOKEN值
-    request.headers.set('x-auth-token', localStorage.getItem('TOKEN'));
-    next((response) => {
-        //登录极验证时需获取后台返回的TOKEN值
-        var xAuthToken = response.headers.get('x-auth-token');
-        if (xAuthToken != null && xAuthToken != '') {
-            localStorage.setItem('TOKEN', xAuthToken);
-        }
-        //
-        if (response.body.code == '4000' || response.body.code == '3000') {
-            store.commit('setMember', null);
-            // router.push('/login');
-            return false;
-        }
-        return response;
-    });
-});
-
-Vue.config.productionTip = false
-Vue.filter('timeFormat', function(tick) {
-    return moment(tick).format("HH:mm:ss");
-});
-Vue.filter('dateFormat', function(tick) {
-    return moment(tick).format("YYYY-MM-DD HH:mm:ss");
-});
-Vue.filter('ymdFormat', function(tick) {
-    return moment(tick).format("YYYY-MM-DD");
-});
-Vue.filter('toFixed', function(number, scale) {
-    return new Number(number).toFixed(scale);
-});
-Vue.filter('toPercent', function(point) {
-    var str = Number(point * 100).toFixed(1);
-    str += "%";
-    return str;
-});
-//数字进行下舍入(舍去)
 function toFloor(number, scale = 8) {
     if (new Number(number) == 0) { //如果是"0.0000000000000000"
         return 0;
@@ -97,32 +36,63 @@ function toFloor(number, scale = 8) {
         return str;
     }
 }
-
 Vue.filter('toFloor', (number, scale) => {
     return toFloor(number, scale);
 });
-// Input 特殊字符限制
-Vue.filter('reg', function(reg) {
-    var reg = new RegExp(/[\-\_\,\!\|\~\`\(\)\#\@\%\-\+\=\/\'\￥\。\ \…\$\（\）\(\)\[\]\【\】\^\&\*\{\}\:\;\"\L\<\>\?\\]/g, '');
-    return reg;
-});
-//数组三位一个逗号的正则匹配
-const threeComma = (num) => {
-    if (num) {
-        return String(num).replace(/(?=(\B)(\d{3})+$)/g, ',');
-    } else {
-        return 0;
-    }
-}
-Vue.filter('threeComma', threeComma);
-
 Vue.prototype.toFloor = toFloor;
+axios.interceptors.response.use((response) => {
+    const data = response.data
+    let baseURL = response.config.baseURL;
+    let url = response.config.url;
+
+    if (data.code === 4000) {
+        Cookies.remove('user');
+        Cookies.remove('userPhone');
+        Cookies.remove('userInfo');
+        router.push({ name: 'login' });
+    }
+    if (data.code === 5000) {
+        router.push({ name: 'error-403' });
+    }
+    return response;
+})
+
+// axios.interceptors.request.use((config) => {
+//   config.headers['x-auth-token'] = Cookies.get('x-auth-token');
+//   return config;
+// })
+Vue.prototype.numToFixed = util.numToFixed; //主要用于科学计数法转数字
+
+Vue.prototype.api = Api;
+Vue.use(VueI18n);
+Vue.use(iView);
+Vue.use(Viser);
 
 new Vue({
     el: '#app',
-    router,
-    i18n,
-    store,
-    template: '<App/>',
-    components: { App }
-})
+    router: router,
+    store: store,
+    render: h => h(App),
+    data: {
+        currentPageName: ''
+    },
+    mounted() {
+        this.currentPageName = this.$route.name;
+        // 显示打开的页面的列表
+        this.$store.commit('setOpenedList');
+        this.$store.commit('initCachepage');
+        // 权限菜单过滤相关
+        this.$store.commit('updateMenulist');
+    },
+    created() {
+        let tagsList = [];
+        appRouter.map((item) => {
+            if (item.children.length <= 1) {
+                tagsList.push(item.children[0]);
+            } else {
+                tagsList.push(...item.children);
+            }
+        });
+        this.$store.commit('setTagsList', tagsList);
+    }
+});
